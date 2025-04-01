c447bb03bf9364f6d9a61a30edc867c4f3816d34
javid shabin <javid.main.act@gmail.com>
2025-04-01 17:25:37 +0530

    add router for the get list function

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -1,11 +1,12 @@
 import express from "express";
-import { createProduct } from "../controllers/productController.js";
+import { createProduct, productList } from "../controllers/productController.js";
 import { upload } from "../middlewares/multer.js";
 
 
 const productRouter = express.Router();
 
 productRouter.post("/create_product", upload.single("image"), createProduct)
+productRouter.get("/product_list", productList)
 
 
 export default productRouter;
