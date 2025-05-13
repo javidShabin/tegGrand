8207bece6875924eaccafd4af6120ad5c606cd3a
javid shabin <javid.main.act@gmail.com>
2025-05-13 17:23:52 +0530

    create the get list of product function

--- controllers/productController.js
+++ controllers/productController.js
@@ -2,6 +2,7 @@ import Product from "../models/productModel.js";
 import cloudinaryInstance from "../configs/cloudinary.js";
 import CustomError from "../utils/customError.js";
 
+// Create new product
 export const createProduct = async (req, res, next) => {
   const { name, price, description, category } = req.body;
 
@@ -11,10 +12,8 @@ export const createProduct = async (req, res, next) => {
       throw new CustomError("All fields including image are required", 400);
 
     // Check the product already exist in database
-    const isProductExist = await Product.findOne({name})
-    if (isProductExist)
-        throw new CustomError("The product is already exists") 
-
+    const isProductExist = await Product.findOne({ name });
+    if (isProductExist) throw new CustomError("The product is already exists");
 
     // Upload image
     let uploadResult;
@@ -43,6 +42,24 @@ export const createProduct = async (req, res, next) => {
       success: true,
       message: "Product created successfully",
       product: newProduct,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+// Get all product list
+export const productList = async (req, res, next) => {
+  try {
+    const products = await Product.find();
+
+    if (!products || products.length === 0) 
+      throw new CustomError("No products found", 404);
+    
+
+    res.status(200).json({
+      success: true,
+      message: "Products fetched successfully",
+      products,
     });
   } catch (error) {
     next(error);
