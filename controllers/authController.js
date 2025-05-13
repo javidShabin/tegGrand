36ddef5f700096c292bcbfb6e21fb883f186396e
javid shabin <javid.main.act@gmail.com>
2025-05-13 15:44:02 +0530

    get all field from request boyd in auth controler register function and check required fileds are present or not

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,19 @@
 import User from "../models/authModel";
 import cloudinaryInstance from "../configs/cloudinary";
 import bcrypt from "bcrypt";
+import CustomError from "../utils/customError.js";
 
 export const registerUser = async (req, res, next) => {
-    const {fullName, email, phone, password,profilePictur} = req.body
-}
+  const { fullName, email, phone, password, confirmPassword, profilePicture } = req.body;
+
+  try {
+    // Check the required fields are present or not
+    if (!fullName || !email || !phone || !password || !confirmPassword) {
+      throw new CustomError('All fields are required', 400);
+    }
+
+    
+  } catch (error) {
+    next(error);
+  }
+};
