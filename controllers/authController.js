2cef727253ee0838a154b977f2127ff47ef7ed97
javid shabin <javid.main.act@gmail.com>
2025-05-13 16:05:44 +0530

    complete the user register function and generate token then set the token in cookie

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,9 +2,10 @@ import User from "../models/authModel.js";
 import cloudinaryInstance from "../configs/cloudinary.js";
 import bcrypt from "bcrypt";
 import CustomError from "../utils/customError.js";
+import { generateUserToken } from "../utils/token.js";
 
 export const registerUser = async (req, res, next) => {
-  const { fullName, email, phone, password, confirmPassword } = req.body;
+  const { fullName, email, phone, password, confirmPassword, profilePicture } = req.body;
 
   try {
     // Check the required fields are present or not
@@ -19,9 +20,37 @@ export const registerUser = async (req, res, next) => {
     if (isUserExist) {
       throw new CustomError("User already exists", 409);
     }
-    // Hash the password using bcrypt 
-    const hashedPassword = await bcrypt.hash(password, 10)
-    console.log(hashedPassword)
+    // Hash the password using bcrypt
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Save the new user
+    const newUser = new User({
+      fullName,
+      email,
+      password,
+      phone,
+      profilePicture,
+    });
+    await newUser.save();
+
+    // Generate user toke
+    const token = generateUserToken({
+      _id: newUser._id,
+      email: newUser.email,
+      role: "user",
+    });
+    // Set the token to cookie
+    res.cookie("userToken", token, {
+      httpOnly: true,
+      secure: false,
+      sameSite: "strict",
+    });
+    // Send success response
+    res.status(201).json({
+      success: true,
+      message: "User registered successfully",
+      user: newUser,
+    });
   } catch (error) {
     next(error);
   }
