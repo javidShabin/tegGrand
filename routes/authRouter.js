3022cc0a75a4ef3c2d05eff96b65a22c6cae1e4d
javid shabin <javid.main.act@gmail.com>
2025-05-13 16:18:16 +0530

    create the route for login user

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { registerUser } from "../controllers/authController.js";
+import { loginUser, registerUser } from "../controllers/authController.js";
 
 const authRouter = express.Router();
 
 authRouter.post("/user_register", registerUser)
+authRouter.post("/user_login", loginUser)
 
 
 export default authRouter;
