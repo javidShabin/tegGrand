2af0903bf0522cf53ae35929d044200785be13db
javid shabin <javid.main.act@gmail.com>
2025-05-13 18:06:43 +0530

    create the middleware for user authentication

---
+++ middlewares/authMiddlware.js
@@ -0,0 +1,17 @@
+import jwt from "jsonwebtoken";
+import CustomError from "../utils/customError";
+export const userAuth = (req, res, next) => {
+  try {
+    // Get token from cookies
+    const { userToken } = req.cookies;
+    // Check user token is present
+    if (!userToken) throw new CustomError("User not authorized0", 401);
+    // verify the token
+    const verifyToken = jwt.verify(userToken, process.env.JWT_SECRET_KEY);
+    if (!verifyToken) throw new CustomError("User not authorized", 401);
+    req.user = verifyToken;
+    next();
+  } catch (error) {
+    next(error);
+  }
+};
