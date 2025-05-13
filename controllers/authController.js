964883b391f363486af60ea019708c4192d396fc
javid shabin <javid.main.act@gmail.com>
2025-05-13 15:40:01 +0530

    create custom error handler class and update my error handling middlware for the calss

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,4 +2,6 @@ import User from "../models/authModel";
 import cloudinaryInstance from "../configs/cloudinary";
 import bcrypt from "bcrypt";
 
-export const registerUser = async ()
+export const registerUser = async (req, res, next) => {
+    const {fullName, email, phone, password,profilePictur} = req.body
+}
--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,13 +1,19 @@
+// middlewares/errorMiddleware.js
 const errorHandler = (err, req, res, next) => {
   const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+
+  if (process.env.NODE_ENV === 'development') {
+    console.error(err.stack);
+  }
 
   res.status(statusCode).json({
     success: false,
-    message: err.message || 'Something went wrong',
+    message,
+    ...(process.env.NODE_ENV === 'development' && { stack: err.stack })
   });
 };
 
 export default errorHandler;
-
 
 
